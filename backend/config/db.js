d39ca09064654ea7ce815db0a91fd3dc13a6aee4
unknown <al03098605@tecmilenio.mx>
2026-02-15 23:52:29 -0600

    Fix SSL for Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    connectTimeout: 10000
 });
 
 module.exports = pool;
